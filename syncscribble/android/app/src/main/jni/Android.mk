1cc55eab718aa23865fc24e82aef42b70c669f1d
Amit Novick <axnovick@gmail.com>
2025-12-06 23:22:37 +0200

    build: fix error

--- syncscribble/android/app/src/main/jni/Android.mk
+++ syncscribble/android/app/src/main/jni/Android.mk
@@ -3,8 +3,6 @@
 # Note that symlinking source dirs is a terrible idea which can create a huge mess when trying to open files,
 #  esp. when debugging
 # Use relative paths instead of absolute paths for CI/CD compatibility
-LOCAL_PATH_BACKUP := $(LOCAL_PATH)
-LOCAL_PATH := $(LOCAL_PATH)/../../../../../..
-include $(LOCAL_PATH)/SDL/Android.mk
-LOCAL_PATH := $(LOCAL_PATH_BACKUP)
+# From jni directory: jni -> main -> src -> app -> android -> syncscribble -> Write (root)
+include $(LOCAL_PATH)/../../../../../../SDL/Android.mk
 include $(LOCAL_PATH)/../../../../../../syncscribble/Makefile
